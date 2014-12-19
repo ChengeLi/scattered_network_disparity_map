ae557f321afe84fdbd17f7c321f1ae858b92df89
ChengeLi <lichenge0223@gmail.com>
2014-12-19 12:35:40 -0500

    First git

---
+++ make.m
@@ -0,0 +1,10 @@
+%MAKE   Compiles the maxflowmex library.
+%   
+%   (c) 2008 Michael Rubinstein, WDI R&D and IDC
+%   $Revision: 130 $
+%   $Date: 2008-09-02 17:10:31 -0700 (Tue, 02 Sep 2008) $
+%
+
+mex -largeArrayDims scattered_network_disparity_map/maxflowmex.cpp...
+                    scattered_network_disparity_map/maxflow-v3.03/graph.cpp...
+                    scattered_network_disparity_map/maxflow-v3.03/maxflow.cpp 
